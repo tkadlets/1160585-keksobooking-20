70ba5c3810e070efc791fe0cdd32731c9334e0b5
Lidia <60134376+tkadlets@users.noreply.github.com>
2020-07-11 01:44:13 +1000

    активация, валидация

--- js/main.js
+++ js/main.js
@@ -70,10 +70,6 @@ var generateData = function () {
 
 var mockData = generateData();
 
-// временно включаем карту
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
 
 // создаем метки
 var createPins = function (data) {
@@ -96,12 +92,156 @@ var createPins = function (data) {
 };
 
 
+// отрисовка похожих объявлений
 var embedPins = function (adsData) {
   var pinFragment = createPins(adsData);
   var mapPins = document.querySelector('.map__pins');
   mapPins.appendChild(pinFragment);
 };
 
-embedPins(mockData);
+
+// неактивный режим страницы:
+// вызов метода отрисовки похожих объявлений нужно перенести в функцию активации && mousedown на .map__pin--main активирует страницу
+// && элементы управления формы (input, select и т. д.) должны быть неактивны в исходном состоянии - добавить через DOM-операции самим полям или fieldset, которые их содержат, атрибут disabled
+var disableAdFields = function () {
+  var adFormFields = document.querySelectorAll('.ad-form input, .ad-form select');
+  for (var i = 0; i < adFormFields.length; i++) {
+    adFormFields[i].setAttribute('disabled', true);
+  }
+};
+
+var disableFilterFields = function () {
+  var filtersFormFields = document.querySelectorAll('.map__filters input, .map__filters select');
+  for (var i = 0; i < filtersFormFields.length; i++) {
+    filtersFormFields[i].setAttribute('disabled', true);
+  }
+};
+
+disableAdFields();
+disableFilterFields();
+
+// переключаемся в активный режим
+var activatePage = function () {
+  embedPins(mockData);
+
+  var map = document.querySelector('.map');
+  map.classList.remove('map--faded');
+
+  var adForm = document.querySelector('.ad-form');
+  adForm.classList.remove('ad-form--disabled');
+
+  var adFormFields = document.querySelectorAll('.ad-form input, .ad-form select');
+  for (var i = 0; i < adFormFields.length; i++) {
+    adFormFields[i].removeAttribute('disabled');
+  }
+
+  var filtersFormFields = document.querySelectorAll('.map__filters input, .map__filters select');
+  for (var j = 0; j < filtersFormFields.length; j++) {
+    filtersFormFields[j].removeAttribute('disabled');
+  }
+};
+
+// заполняем адрес
+
+var setAddress = function () {
+  var map = document.querySelector('.map');
+  var mainPin = document.querySelector('.map__pin--main');
+  var addressField = document.querySelector('#address');
+  if (map.classList.contains('map--faded')) {
+    var mainPinLocationX = parseInt(mainPin.style.left, 10) + (parseInt(mainPin.offsetWidth, 10) / 2);
+    var mainPinLocationY = parseInt(mainPin.style.top, 10) + (parseInt(mainPin.offsetWidth, 10) / 2);
+  } else {
+    mainPinLocationX = parseInt(mainPin.style.left, 10) + (parseInt(mainPin.offsetWidth, 10) / 2);
+    mainPinLocationY = parseInt(mainPin.style.top, 10) + (parseInt(mainPin.offsetWidth, 10));
+  }
+  addressField.value = Math.round(mainPinLocationX) + ', ' + Math.round(mainPinLocationY);
+};
+
+setAddress();
+
+var mainPin = document.querySelector('.map__pin--main');
+mainPin.addEventListener('mousedown', function (evt) {
+  if (evt.button === 0) {
+    activatePage();
+    setAddress();
+  }
+});
+
+mainPin.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    activatePage();
+    setAddress();
+  }
+});
 
 
+// валидируем ad-form
+var adType = document.querySelector('#type');
+var adPrice = document.querySelector('#price');
+
+var rangePrice = function () {
+
+  if (adType.value === 'bungalo') {
+    adPrice.setAttribute('min', '0');
+    adPrice.placeholder = '0';
+  } else if (adType.value === 'flat') {
+    adPrice.setAttribute('min', '1000');
+    adPrice.placeholder = '1000';
+  } else if (adType.value === 'house') {
+    adPrice.setAttribute('min', '5000');
+    adPrice.placeholder = '5000';
+  } else if (adType.value === 'palace') {
+    adPrice.setAttribute('min', '10000');
+    adPrice.placeholder = '10000';
+  }
+};
+
+rangePrice();
+adType.addEventListener('change', function () {
+  rangePrice();
+});
+
+
+var checkInTime = document.querySelector('#timein');
+var checkOutTime = document.querySelector('#timeout');
+var syncByCheckIn = function () {
+  checkOutTime.value = checkInTime.value;
+};
+var syncByCheckOut = function () {
+  checkInTime.value = checkOutTime.value;
+};
+
+checkInTime.addEventListener('change', function () {
+  syncByCheckIn();
+});
+
+checkOutTime.addEventListener('change', function () {
+  syncByCheckOut();
+});
+
+
+var guestsNumber = document.querySelector('#capacity');
+var roomsNumber = document.querySelector('#room_number');
+guestsNumber.addEventListener('change', function () {
+  if (roomsNumber.value !== '100' && guestsNumber.value === '0') {
+    guestsNumber.setCustomValidity('Гостей должно быть больше 0');
+  } else if (roomsNumber.value < '100' && guestsNumber.value > roomsNumber.value) {
+    guestsNumber.setCustomValidity('Гостей не может быть больше, чем комнат');
+  } else if (roomsNumber.value === '100' && guestsNumber.value !== '0') {
+    guestsNumber.setCustomValidity('В выбранном типе жилья гостей быть не должно');
+  } else {
+    guestsNumber.setCustomValidity('');
+  }
+});
+
+roomsNumber.addEventListener('change', function () {
+  if (roomsNumber.value !== '100' && guestsNumber.value === '0') {
+    guestsNumber.setCustomValidity('Гостей должно быть больше 0');
+  } else if (roomsNumber.value < '100' && guestsNumber.value > roomsNumber.value) {
+    guestsNumber.setCustomValidity('Гостей не может быть больше, чем комнат');
+  } else if (roomsNumber.value === '100' && guestsNumber.value !== '0') {
+    guestsNumber.setCustomValidity('В выбранном типе жилья гостей быть не должно');
+  } else {
+    guestsNumber.setCustomValidity('');
+  }
+});
